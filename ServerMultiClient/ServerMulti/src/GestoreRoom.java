d9bcbbdb757e3f4cb68637b166841fe8d8c4005a
Mazzo-007 <mazzoni.luca@studenti.salesianisesto.it>
2025-05-23 10:41:53 +0200

    ServerMultiClient_v1.0.0

---
+++ ServerMultiClient/ServerMulti/src/GestoreRoom.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class GestoreRoom {
+    private HashMap<String, Room> stanze;
+
+    public GestoreRoom () {
+        stanze = new HashMap<>();
+    }
+
+    public boolean addRoom (String ID, Room room) {
+        if (stanze.containsKey(ID)) {
+            return false;
+        }
+        stanze.put(ID, room);
+        return true;
+    }
+
+    public boolean deleteRoom (String ID) {
+        if (stanze.containsKey(ID)) {
+            return false;
+        }
+        stanze.remove(ID);
+        return true;
+    }
+}
---
+++ ServerMultiClient/ServerMulti/src/roomSelector.java
@@ -0,0 +1,3 @@
+public class roomSelector extends Thread {
+
+}
