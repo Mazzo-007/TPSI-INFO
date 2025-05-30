7e00dd434d65a2de9bfa7489eb7a554d27348919
Mazzo-007 <mazzoni.luca@studenti.salesianisesto.it>
2025-05-30 09:45:27 +0200

    ServerMultiClient_Start

--- ServerMultiClient/ServerMulti/src/room/Room.java
+++ ServerMultiClient/ServerMulti/src/room/Room.java
@@ -1,3 +1,7 @@
+package room;
+
+import client.Client;
+
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Vector;
@@ -9,23 +13,14 @@ import java.util.Vector;
 
 public class Room{
     private Vector<Client> lista;
-    private String ID;
-    private GestoreRoom gr;
 
-    public Room(String ID, GestoreRoom gr) {
-        this.ID = ID;
+    public Room() {
         lista = new Vector<>();
-        this.gr = gr;
-    }
-
-    public String getID() {
-        return ID;
     }
 
     public void addClient(Socket s) throws IOException, InterruptedException {
         lista.add(new Client(s,this));
     }
-
 
     public void trasmetti(String sms,Client utente){
         for(Client c : lista){
@@ -33,19 +28,7 @@ public class Room{
                 c.send(sms);
         }
     }
-
     public void abbandona(Client c){
-        if(lista.remove(c)) {
-            Room r = gr.getRoom("INFO");
-            r.addInfo(r,c);
-        }
-    }
-
-    private void addInfo(Room info, Client c){
-        info.getLista().add(c);
-    }
-
-    public Vector<Client> getLista(){
-        return lista;
+        lista.remove(c);
     }
 }
