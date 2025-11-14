ad1993f7e9c2747572e36e7281b0c518fd847bb3
Mazzoni Luca <mazzoni.luca@osdbsesto>
2025-11-14 15:59:07 +0100

    TomatoTimer_v2.0.0

--- WebDev/TomatoTimer/Scripts/tomato.js
+++ WebDev/TomatoTimer/Scripts/tomato.js
@@ -2,6 +2,7 @@ let display = document.getElementById("timer")
 let startBtn = document.getElementById("startBtn")
 let resetBtn = document.getElementById("resetBtn")
 let pauseBtn = document.getElementById("pauseBtn")
+let interval;
 
 let timer = {
     seconds: 1500,
@@ -23,10 +24,24 @@ function tick() {
 function start() {
     if(!timer.running) {
         timer.running = true;
-        setInterval(tick, 1000)
+        interval = setInterval(tick, 1000)
     }
+}
+
+function reset() {
+    clearInterval(interval)
+    timer.seconds = 1500
+    timer.running = false
+    updateView()
+}
+
+function pause() {
+    clearInterval(interval)
+    timer.running = false
 }
 
 // Se la funzione è corta allora lamda
 // altrimenti normale
 startBtn.addEventListener("click", start)
+resetBtn.addEventListener("click", reset)
+pauseBtn.addEventListener("click", pause)
