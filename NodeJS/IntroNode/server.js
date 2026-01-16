362637caee5eb6329117ff90c9c244dc9c303684
Mazzoni Luca <mazzoni.luca@osdbsesto>
2026-01-16 15:27:38 +0100

    IntroNode_v1.0.0

---
+++ NodeJS/IntroNode/server.js
@@ -0,0 +1,25 @@
+// PRO TIP CTR + J
+
+// libreria HTTP
+const http = require("http")
+
+const PORT = 80
+const HOSTNAME = "localhost"
+
+const server = http.createServer((req, res) => {
+
+    if(req.url === "/") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type","text/plain");
+        res.end("ciaoo, benvenuto!");
+    }else {
+        res.statusCode = 404;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ error: req.url + " Not found"}))
+    }
+
+})
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log("ONLINE su http:/" + HOSTNAME + ":" + PORT)
+})
