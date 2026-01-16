49414c51ff587e596fa7f54b82ba0345e8f3e4d8
Mazzoni Luca <mazzoni.luca@osdbsesto>
2026-01-16 16:07:39 +0100

    IntoNode_v1.1.0

--- NodeJS/IntroNode/server.js
+++ NodeJS/IntroNode/server.js
@@ -6,17 +6,53 @@ const http = require("http")
 const PORT = 80
 const HOSTNAME = "localhost"
 
+const user = [
+    { età: 1, username: "Mario Rossi", email: "mario.rossi@gmail.com" },
+    { età: 2, username: "Luigi Bianchi", email: "luigi.bianchi@gmail.com" },
+    { età: 3, username: "Giulia Verdi", email: "giulia.verdi@gmail.com" },
+    { età: 4, username: "Anna Neri", email: "anna.neri@gmail.com" },
+    { età: 5, username: "Paolo Russo", email: "paolo.russo@gmail.com" }
+]
+
 const server = http.createServer((req, res) => {
 
-    if(req.url === "/") {
+    if(req.url === "/" && req.method === "GET") {
         res.statusCode = 200;
         res.setHeader("Content-Type","text/plain");
-        res.end("ciaoo, benvenuto!");
-    }else {
-        res.statusCode = 404;
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify({ error: req.url + " Not found"}))
-    }
+        return res.end("ciaoo, benvenuto!");
+    } 
+    if(req.url === "/" && req.method !== "GET") {
+        res.statusCode = 405; //Method not allowd
+        res.setHeader("Allow", "GET");
+        return res.end();
+    } 
+    
+    if(req.url === "/users" && req.method === "GET") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type","text/plain");
+        return res.end(JSON.stringify(user));
+    } 
+    if(req.url === "/users" && req.method !== "GET") {
+        res.statusCode = 405; //Method not allowd
+        res.setHeader("Allow", "GET");
+        return res.end();
+    } 
+
+    if(req.url === "/number" && req.method === "GET") {
+        res.statusCode = 200;
+        res.setHeader("Content-Type","text/plain");
+        const random = Math.floor(Math.random() * 100) + 1;
+        return res.end(String(random));
+    } 
+    if(req.url === "/number" && req.method !== "GET") {
+        res.statusCode = 405; //Method not allowd
+        res.setHeader("Allow", "GET");
+        return res.end();
+    } 
+
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: req.url + " Not found"}))
 
 })
 
