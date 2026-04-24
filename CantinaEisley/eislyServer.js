13f3f7b1a09d18bc3b51e13e0fa6dec1e1fc6384
Mazzo-007 <mazzoni.luca@studenti.salesianisesto.it>
2026-04-24 16:09:31 +0200

    CantinaEisley_v2.0.0

--- CantinaEisley/eislyServer.js
+++ CantinaEisley/eislyServer.js
@@ -1,5 +1,4 @@
 const express = require("express"); //import
-const { log } = require("node:console");
 
 const PORT = 3000;
 
@@ -7,6 +6,72 @@ const PORT = 3000;
 const server = express()
 
 server.use(express.json())
+
+const bevande = [
+    {
+        id : 1,
+        gradazione : 10,
+        nome : "keller",
+        costo : 10 
+    },
+    {
+        id : 2,
+        gradazione : 5,
+        nome : "ipa",
+        costo : 12 
+    },
+    {
+        id : 3,
+        gradazione : 20,
+        nome : "original",
+        costo : 8
+    },
+    {
+        id : 4,
+        gradazione : 15,
+        nome : "ottakringer",
+        costo : 7
+    },
+    {
+        id : 1,
+        gradazione : 10,
+        nome : "manchester bitter",
+        costo : 15
+    }
+];
+
+const cliente = [
+    {
+        id : 1,
+        nome : "Han Solo",
+        specie : "umano",
+        credito : 1500
+    },
+    {
+        id : 2,
+        nome : "Greedo",
+        specie : "rodiano",
+        credito : 300
+    },
+    {
+        id : 3,
+        nome : "Chewbacca",
+        specie : "wookiee",
+        credito : 900
+    },
+    {
+        id : 4,
+        nome : "Hammerhead",
+        specie : "ithoriano",
+        credito : 200
+    },
+    {
+        id : 1,
+        nome : "Roda",
+        specie : "rodiano",
+        credito : 2000
+    }
+];
 
 //MW globale che stampa info per ogni richiesta (REQ)
 server.use( (req, res, next) => {
@@ -43,8 +108,56 @@ server.use("/clienti", (req, res, next) => {
     next()
 })
 
+server.use("/clienti", (req, res, next) => {
+    // leggere ruolo
+    let ruolo = req.headers["x-ruolo"]
+    // se esiste associa valore
+    if (ruolo) {
+        req.ruolo = ruolo
+    } else {
+        req.ruolo = "ospite"
+    }
+
+    next()
+})
+
+server.use("/bevande", (req, res, next) => {
+    // leggere gradazione max
+    let gradazione = parseInt(req.headers["x-gradazione-max"])
+    // se esiste associa il valore
+    if (gradazione) {
+        req.gradazioneMax = gradazione
+    } else {
+        req.gradazioneMax = null
+    }
+
+    next()
+})
+
 server.get("/clienti", (req, res) => {
     res.status(200).json({msg: "test"})
+})
+
+server.get("/clienti/:id/ordini", (req, res) => {
+    let id = parseInt(req.params.id)
+    if (id) {
+
+    } else {
+        res.status(400).json({error: "Accesso non autorizzato"})
+    }
+})
+
+server.get("/bevande", (req, res) => {
+    let gradazioneMax = req.gradazioneMax;
+    if (!gradazioneMax) {
+        res.status(200).json(bevande);
+    } else {
+        let temp = [];
+        for (let bevanda of bevande) {
+            if (bevanda.gradazione <= gradazioneMax) temp.push(bevanda)
+        }
+        res.status(200).json(temp)
+    }
 })
 
 //listen
